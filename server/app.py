c7d1f4ea8af189cf259d3e469a889f939167ffba
John <johntaykimani@gmail.com>
2024-04-10 11:16:11 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -52,20 +52,39 @@ def game_by_id(id):
 
     return response
 
-@app.route('/reviews')
+@app.route('/reviews', methods=['GET', 'POST'])
 def reviews():
+    
+    if request.method == 'GET':
+        reviews = []
+        for review in Review.query.all():
+            review_dict = review.to_dict()
+            reviews.append(review_dict)
 
-    reviews = []
-    for review in Review.query.all():
-        review_dict = review.to_dict()
-        reviews.append(review_dict)
+        response = make_response(
+            reviews,
+            200
+        )
+        return response
+# use elif instead of else because of returning errors like 404 et al.   
+    elif request.method == 'POST':
+        new_review = Review(
+            score=request.form.get('score'),
+            comment=request.form.get('comment'),
+            game_id=request.form.get('game_id'),
+            user_id=request.form.get('user_id'),
+        )
+# Commit Changes to db
+        db.session.add(new_review)
+        db.session.commit()
+# Convert to json and return resp of newly created review
+# We create review_dict after committing the review to the database,
+# As this populates it with an ID and data from its game and user
+        review_dict = new_review.to_dict()
+        resp = make_response(review_dict, 201)
+        print(resp)
 
-    response = make_response(
-        reviews,
-        200
-    )
-
-    return response
+        return resp
 
 @app.route('/users')
 def users():
@@ -81,6 +100,39 @@ def users():
     )
 
     return response
+
+@app.route('/reviews/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
+def review_by_id(id):
+    review = Review.query.filter(Review.id == id).first()
+
+    if request.method == 'GET':
+        review_dict = review.to_dict()
+
+        resp = make_response(review_dict, 200)
+        return resp
+    
+    elif request.method == 'DELETE':
+        db.session.delete(review)
+        db.session.commit()
+
+        repsonse_body = {
+            "delete_successful": True,
+            'message': "Review Deleted!"
+        }
+        resp = make_response(repsonse_body, 200)
+        return resp
+    
+    elif request.method == 'PATCH':
+        for attr in request.form:
+            setattr(review, attr, request.form.get(attr))
+
+        db.session.add(review)
+        db.session.commit()
+
+        review_dict = review.to_dict()
+        resp = make_response(review_dict, 200)
+
+        return resp
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
